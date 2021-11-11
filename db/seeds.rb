8f1ebbb103e60d67f7b64ae0dfd7725b4d9dc00e
Ethan Hardwick <hardwick.ethan@outlook.com>
2021-11-11 15:00:20 +0000

    complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+movie_serialized = URI.open(url).read
+movie = JSON.parse(movie_serialized)
+
+movies = movie['results']
+
+base_url = 'https://image.tmdb.org/t/p/w500'
+
+movies.each do |m|
+  Movie.create(title: m['title'], overview: m['overview'], poster_url: "#{base_url}#{m['poster_path']}", rating: m['vote_average'])
+end
